b99b2be4b08d8f6444879f7c80cf0b4871fce35a
merve cakiroglu <mrveeckrrr@hotmail.com>
2024-08-18 23:56:02 +0300

    Add project files.

---
+++ Week5Practice5TryCatch/Program.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+    public static void Main(string[] args)
+    {
+		try
+		{
+            Console.WriteLine("Bir sayı giriniz : ");
+			int number = Convert.ToInt32(Console.ReadLine());                   //Girilen sayı kullanıcıdan alındı
+            int numberSquare = number * number;                                 //Karesi hesaplanıp bir değişkene atandı
+
+            Console.WriteLine("Girdiğiniz sayının karesi : " + numberSquare);
+
+        }
+		catch (Exception)
+		{
+
+            Console.WriteLine("Geçersiz giriş! Lütfen bir sayı giriniz.");      //Eğer kullanıcı sayısal değer girmezse hata mesajı alır
+        }   
+    }
+}
